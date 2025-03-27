d9891cbbc11b9d55717fed9efc35fd9b75ce6de6
Vincent <vincenttueminh@gmail.com>
2025-03-27 13:51:03 -0400

    template

---
+++ Template.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Input inputs = new Input();
+
+    System.out.print( "Name : " );
+    String name = inputs.letters();
+    System.out.print( "what is your âge ? " );
+    double age = inputs.numbers();
+    System.out.println( name + ", Your are " + age + " years old" );
+  }
+}
+
+
+class Input {
+  public String letters() {
+    Scanner text_input = new Scanner(System.in);
+    return text_input.nextLine();
+  }
+  public double numbers() {
+    Scanner text_input = new Scanner(System.in);
+    return text_input.nextDouble();
+  }
+}
