64abcafbda58b920a4206a399f5040c9caa2fb1e
Zack <4400255@365.csh.qc.ca>
2025-04-14 15:31:06 -0400

    ajout division multiplication vérifier déclaration de classe

--- Template.java
+++ Template.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Basic basic = new Basic();
+    Secondary secondary =new Secondary();
     Input input = new Input();
-    System.out.println("Addition ou Soustraction (entrez la première lettre)");
+    System.out.println("Addition, Soustraction, Multiplication ou Division (entrez la première lettre)");
     String choix_de_reponse = input.letters();
     if( choix_de_reponse.equals("a") ){
       System.out.println("Quel est votre premier nombre?");
@@ -22,7 +23,22 @@ class Main {
       double resultat = basic.soustraction(premier_terme, deuxieme_terme);
       System.out.println("Le résultat est:"+resultat);
     }
-  }
+    else if(choix_de_reponse.equals("m")){
+      System.out.println("Quel est votre premier nombre?");
+      double premier_terme = input.numbers();
+      System.out.println("Quel est le deuxième nombre?");
+      double deuxieme_terme = input.numbers();
+      double resultat = secondary.multiplication(premier_terme, deuxieme_terme);
+      System.out.println("Le résultat est:"+resultat);
+    }
+    else if(choix_de_reponse.equals("d")){
+      System.out.println("Quel est votre nombre?");
+      double premier_terme = input.numbers();
+      System.out.println("Par quoi voulez-vous le diviser?");
+      double deuxieme_terme = input.numbers();
+      double resultat = secondary.division(premier_terme, premier_terme);
+      System.out.println("Le résultat est:"+resultat);
+    }
 }
 
 
@@ -47,4 +63,21 @@ class Basic {
     double c = a-b;
     return c;
   }
+}
+
+
+class Secondary {
+  public double multiplication(double a, double b){
+    double c=a*b;
+    return c;
+  }
+  public double division(double a, double b){
+    if(b == 0) {
+      return 0;
+    }
+    double c=a/b;
+    return c;
+
+  }
+
 }
