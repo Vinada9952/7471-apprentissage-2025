cc014688aa18a69823b663bba0e3da4f277d6b5e
Zack <4400255@365.csh.qc.ca>
2025-04-14 15:46:59 -0400

    to correct

--- Template.java
+++ Template.java
@@ -5,40 +5,50 @@ class Main {
     Basic basic = new Basic();
     Secondary secondary =new Secondary();
     Input input = new Input();
-    System.out.println("Addition, Soustraction, Multiplication ou Division (entrez la première lettre)");
-    String choix_de_reponse = input.letters();
-    if( choix_de_reponse.equals("a") ){
-      System.out.println("Quel est votre premier nombre?");
-      double premier_terme = input.numbers();
-      System.out.println("Quel est le deuxième nombre?");
-      double deuxieme_terme = input.numbers();
-      double resultat = basic.addition(premier_terme, deuxieme_terme);
-      System.out.println("Le résultat est:"+resultat);
+    while(true){
+      
+      System.out.println("Addition, Soustraction, Multiplication ou Division (entrez la première lettre)");
+      String choix_de_reponse = input.letters().toLowerCase();
+      if( choix_de_reponse.equals("a") ){
+        System.out.println("Quel est votre premier nombre?");
+        double premier_terme = input.numbers();
+        System.out.println("Quel est le deuxième nombre?");
+        double deuxieme_terme = input.numbers();
+        double resultat = basic.addition(premier_terme, deuxieme_terme);
+        System.out.println("Le résultat est:"+resultat);
+      }
+      else if(choix_de_reponse.equals("s")){
+        System.out.println("Quel est votre premier nombre?");
+        double premier_terme = input.numbers();
+        System.out.println("Quel est le deuxième nombre?");
+        double deuxieme_terme = input.numbers();
+        double resultat = basic.soustraction(premier_terme, deuxieme_terme);
+        System.out.println("Le résultat est:"+resultat);
+      }
+      else if(choix_de_reponse.equals("m")){
+        System.out.println("Quel est votre premier nombre?");
+        double premier_terme = input.numbers();
+        System.out.println("Quel est le deuxième nombre?");
+        double deuxieme_terme = input.numbers();
+        double resultat = secondary.multiplication(premier_terme, deuxieme_terme);
+        System.out.println("Le résultat est:"+resultat);
+      }
+      else if(choix_de_reponse.equals("d")){
+        System.out.println("Quel est votre nombre à diviser?");
+        double premier_terme = input.numbers();
+        System.out.println("Par quoi voulez-vous le diviser?");
+        double deuxieme_terme = input.numbers();
+        double resultat = secondary.division(premier_terme, premier_terme);
+        System.out.println("Le résultat est:"+resultat);
+      }
+
+      System.out.println("Voulez-vous effectuer un autre calcul?");
+      String confirmation = input.letters().toLowerCase();
+      if(confirmation.equals("non")){
+        break;
+      }
     }
-    else if(choix_de_reponse.equals("s")){
-      System.out.println("Quel est votre premier nombre?");
-      double premier_terme = input.numbers();
-      System.out.println("Quel est le deuxième nombre?");
-      double deuxieme_terme = input.numbers();
-      double resultat = basic.soustraction(premier_terme, deuxieme_terme);
-      System.out.println("Le résultat est:"+resultat);
-    }
-    else if(choix_de_reponse.equals("m")){
-      System.out.println("Quel est votre premier nombre?");
-      double premier_terme = input.numbers();
-      System.out.println("Quel est le deuxième nombre?");
-      double deuxieme_terme = input.numbers();
-      double resultat = secondary.multiplication(premier_terme, deuxieme_terme);
-      System.out.println("Le résultat est:"+resultat);
-    }
-    else if(choix_de_reponse.equals("d")){
-      System.out.println("Quel est votre nombre?");
-      double premier_terme = input.numbers();
-      System.out.println("Par quoi voulez-vous le diviser?");
-      double deuxieme_terme = input.numbers();
-      double resultat = secondary.division(premier_terme, premier_terme);
-      System.out.println("Le résultat est:"+resultat);
-    }
+  }
 }
 
 
@@ -77,7 +87,6 @@ class Secondary {
     }
     double c=a/b;
     return c;
-
   }
 
 }
