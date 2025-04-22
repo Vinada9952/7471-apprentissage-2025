08e8fbbc11df3b66a42cbd4fc46dc05e32c750a8
Olivier <Olivier>
2025-04-22 18:12:57 -0400

    Calcu + et -

--- Template.java
+++ Template.java
@@ -6,11 +6,22 @@ class Main {
     Basics basics = new Basics();
     System.out.println("Entrez votre premier nombre : ");
     double a = input.numbers();
-    System.out.println("Quelle operation voulez-vous faire?")
+    System.out.println("Quelle operation voulez-vous faire? 1. Addition(+) 2. Soustraction(-)");
+    double choix = input.numbers();
     System.out.println("Entrez votre deuxieme nombre : ");
     double b = input.numbers();
-    double c = basics.add(a, b);
-    System.out.println("Le résultat est " + c);
+    double c = 0;
+    if( choix == 1 ){
+      c = basics.add(a, b);
+      System.out.println("Le résultat est " + c);
+    }
+    else if( choix == 2 ) {
+      c = basics.moins(a, b);
+      System.out.println("Le résultat est " + c);
+    }
+    else {
+      System.out.println("operateur non-valide");
+    }
   }
 }
 
