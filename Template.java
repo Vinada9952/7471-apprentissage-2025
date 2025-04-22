8b83308ea5a85708513e027f8f8fe5c3f8ac664b
Vincent <vincenttueminh@gmail.com>
2025-04-22 17:12:01 -0400

    ce que Olivier va faire

--- Template.java
+++ Template.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Olivier edited this file");
+    System.out.println("Olivier edited this file and will make it something great");
     System.out.println( "Olivier" );
     Input inputs = new Input();
 
