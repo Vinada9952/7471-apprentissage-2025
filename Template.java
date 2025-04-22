6ab75e1e92fe10d5432b2548cfcde4a438b0657b
Olivier <Olivier>
2025-04-22 17:10:37 -0400

    Vincent n'as pas éditer ce fichier

--- Template.java
+++ Template.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Vincent edited this file");
+    System.out.println("Olivier edited this file");
     System.out.println( "Olivier" );
     Input inputs = new Input();
 
